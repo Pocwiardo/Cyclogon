70cb37a3a5fc2b33a2809b75e65cd54325378f72
Pocwiardo <80002864+Pocwiardo@users.noreply.github.com>
2022-12-12 22:03:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,30 @@ def funkcja(x):
     return 10*np.sin(x/5)
 #    return x**2 /10
 
-#func_formula = input("Enter the function formula: ")
+#func_formula = input("Wpisz wzór funkcji: ")
 
-# create a lambda function from the formula
 #funkcja = lambda x: eval(func_formula)
 
 #x = np.linspace(0,20,21)
-x = np.arange(-10,10, np.pi/100)
-y = funkcja(x)
+#normalna funkcja
+#x = np.arange(-10,10, np.pi/100)
+#y = funkcja(x)
+param = np.linspace(0,2*np.pi,1000)
+x = 8*np.cos(param)
+y = 5*np.sin(param)
 dx = np.diff(x)
 dy = np.diff(y)
 katnach = np.arctan2(dy,dx)
 #pochodna = np.gradient(y,x)
 fig, ax = plt.subplots()
-#ax.set_xlim(0, 10)
-#ax.set_ylim(funkcja(0), funkcja(10))
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
 #ax.set_ylim(-4, 4)
 #if(funkcja(10) < funkcja(0) -1 ):
 #    ax.invert_yaxis()
 #ax.set_aspect('equal')
 
-line = ax.plot(x,y)
+line, = ax.plot(x,y)
 kolo, = ax.plot(0,0)
 promien, = ax.plot(0,0)
 sladx = []
@@ -44,11 +47,12 @@ def frame(i):
     korekcjay = r*np.cos(katnach[i])
     #krokxprev = krokx
     #krokyprev = kroky
-    xokregu = r*np.cos(t) + krokx + korekcjax
-    yokregu = r*np.sin(t) + kroky + korekcjay
+    kat = - np.pi * i / dzielnik
+    xokregu = r*np.cos(t+kat) + krokx + korekcjax
+    yokregu = r*np.sin(t+kat) + kroky + korekcjay
     kolo.set_xdata(xokregu)
     kolo.set_ydata(yokregu)
-    kat = - np.pi * i/dzielnik
+
     xwodz = r*np.cos(kat) + krokx + korekcjax
     ywodz = r*np.sin(kat) + kroky + korekcjay
     promien.set_xdata([krokx + korekcjax, xwodz])
@@ -58,5 +62,5 @@ def frame(i):
     slad.set_data(sladx, slady)
     return kolo, promien, slad
 
-animation = animation.FuncAnimation(fig, func=frame, frames=np.arange(len(x) - 1), interval=1, repeat=False)
+animation = animation.FuncAnimation(fig, frame, frames=np.arange(len(x) - 1), interval=1, repeat=False)
 plt.show()
